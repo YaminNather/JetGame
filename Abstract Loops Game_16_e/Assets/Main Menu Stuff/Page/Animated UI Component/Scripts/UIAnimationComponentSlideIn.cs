51669c092bc527b75398d1b086c41a91b6e64ae1
Yamin Nather <2001s.yn@gmail.com>
2020-10-08 13:17:20 +0530

    Updated the Main Page to the new Design.
    Updated the Main Page to the new Design, colored outline and color filled icons.

--- Abstract Loops Game_16_e/Assets/Main Menu Stuff/Page/Animated UI Component/Scripts/UIAnimationComponentSlideIn.cs
+++ Abstract Loops Game_16_e/Assets/Main Menu Stuff/Page/Animated UI Component/Scripts/UIAnimationComponentSlideIn.cs
@@ -9,34 +9,34 @@ public class UIAnimationComponentSlideIn : UIAnimationComponent
     #region Variables
     [Header("Sliding Settings")]
     [SerializeField] private SlideInDir_EN m_SlideInDir;   
-    private Vector3 m_PosInit;
+    private Vector3 m_AnchoredPosInit;
     #endregion
 
     public override void OnSceneLoaded_F()
     {
-        m_PosInit = GetComponent<RectTransform>().localPosition;
+        m_AnchoredPosInit = GetComponent<RectTransform>().anchoredPosition;
     }    
 
     public override void EntryInitialize_F()
     {
-        GetComponent<RectTransform>().localPosition = ScreenOutPosCalc_F();
+        GetComponent<RectTransform>().anchoredPosition = ScreenOutPosCalc_F();
     }
     
     public override Tweener EntryTween_F()
     {
-        Tweener T_0 = DOTween.To(() => ScreenOutPosCalc_F(), val => GetComponent<RectTransform>().localPosition = val, m_PosInit, Duration);
+        Tweener T_0 = DOTween.To(() => ScreenOutPosCalc_F(), val => GetComponent<RectTransform>().anchoredPosition = val, m_AnchoredPosInit, Duration);
         EntryTweenSetEase_F(T_0);
         return T_0;
     }
 
     public override void ExitInitialize_F()
     {
-        GetComponent<RectTransform>().localPosition = m_PosInit;   
+        GetComponent<RectTransform>().anchoredPosition = m_AnchoredPosInit;   
     }
     
     public override Tweener ExitTween_F()
     {
-        Tweener T_0 = DOTween.To(() => m_PosInit, val => GetComponent<RectTransform>().localPosition = val, ScreenOutPosCalc_F(), Duration);
+        Tweener T_0 = DOTween.To(() => m_AnchoredPosInit, val => GetComponent<RectTransform>().anchoredPosition = val, ScreenOutPosCalc_F(), Duration);
         ExitTweenSetEase_F(T_0);
         return T_0;
     }
@@ -46,19 +46,19 @@ public class UIAnimationComponentSlideIn : UIAnimationComponent
         switch(m_SlideInDir)
         {
             case SlideInDir_EN.Right:
-                return m_PosInit + new Vector3(Screen.width / 2, 0f, 0f);
+                return m_AnchoredPosInit + new Vector3(Screen.width * 2, 0f, 0f);
 
             case SlideInDir_EN.Left:
-                return m_PosInit + new Vector3(-Screen.width / 2, 0f, 0f);
+                return m_AnchoredPosInit + new Vector3(-Screen.width * 2, 0f, 0f);
 
             case SlideInDir_EN.Up:
-                return m_PosInit + new Vector3(0f, Screen.height / 2, 0f);
+                return m_AnchoredPosInit + new Vector3(0f, Screen.height * 2, 0f);
 
             case SlideInDir_EN.Down:
-                return m_PosInit + new Vector3(0f, -Screen.height / 2, 0f);
+                return m_AnchoredPosInit + new Vector3(0f, -Screen.height * 2, 0f);
 
             default:
-                return m_PosInit;
+                return m_AnchoredPosInit;
 
         }
     }
