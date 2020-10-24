b8019d900d6bf46e0d77f7dfb0f24d1d77acb7a5
Yamin Nather <2001s.yn@gmail.com>
2020-10-24 15:05:51 +0530

    Added Functionality to Settings Slider.
    Added Functionality to Settings Slider.

--- Abstract Loops Game_16_e/Assets/Main Menu Stuff/Settings Page/Scripts/SettingsPageMgr.cs
+++ Abstract Loops Game_16_e/Assets/Main Menu Stuff/Settings Page/Scripts/SettingsPageMgr.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 
 public class SettingsPageMgr : MonoBehaviour
@@ -9,7 +10,20 @@ public class SettingsPageMgr : MonoBehaviour
     [Header("UI References")]
     [SerializeField] private Slider m_MusicSlider;
     [SerializeField] private Slider m_SFXSlider;
+
+    [Header("AudioMixer")]
+    [SerializeField] private AudioMixer m_0AudioMixer;
     #endregion
+
+    private void OnEnable()
+    {
+        float volumeFromMixer = 0.0f;
+        m_0AudioMixer.GetFloat("MusicVolume", out volumeFromMixer);
+        m_MusicSlider.SetValueWithoutNotify(volumeFromMixer);
+
+        m_0AudioMixer.GetFloat("SFXVolume", out volumeFromMixer);
+        m_SFXSlider.SetValueWithoutNotify(volumeFromMixer);
+    }
 
     private void Start()
     {
@@ -17,9 +31,10 @@ public class SettingsPageMgr : MonoBehaviour
         m_SFXSlider.onValueChanged.AddListener(SFX_SEF);
     }
 
-    #region UI Event Functions
-    public void Music_SEF(float value) => Debug.Log($"Music Value = {value}");
 
-    public void SFX_SEF(float value) => Debug.Log($"SFX Value = {value}");
+    #region UI Event Functions
+    public void Music_SEF(float value) => m_0AudioMixer.SetFloat("MusicVolume", value);
+
+    public void SFX_SEF(float value) => m_0AudioMixer.SetFloat("SFXVolume", value);
     #endregion
 }
