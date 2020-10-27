71312bfc18c819debe1d4dcaba715a885ac2e58c
Yamin Nather <2001s.yn@gmail.com>
2020-10-27 08:13:59 +0530

    Created an FPS Display Component to view the game's FPS.
    Created an FPS Display Component to view the game's FPS.

---
+++ Abstract Loops Game_16_e/Assets/FPS Display/Scripts/FPSDisplayComponent.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class FPSDisplayComponent : MonoBehaviour
+{
+    private Text m_Text;
+
+    private void Awake()
+    {
+        m_Text = GetComponent<Text>();
+        StartCoroutine(LabelUpdate_IEF());
+    }
+
+    private IEnumerator LabelUpdate_IEF()
+    {
+        WaitForSeconds wfs = new WaitForSeconds(1.0f);
+        while (true)
+        {
+            m_Text.text = "" + (1.0f / Time.deltaTime);
+            yield return wfs;
+        }
+    }
+}
